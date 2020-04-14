5ce13fc6b2c85c786aa0b263fda1e24e448d6ad9
Rajkiran Gurunathan (TA-14) <Rajkiran.GA.Gurunathan@bmw.de>
2020-04-15 00:24:44 +0200

    add search results

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <Link to="/">
           <h1>Home</h1>
         </Link>
-        <Search/>
+        <Search path="/"/>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Search/);
+  const linkElement = getByText(/Loading.../);
   expect(linkElement).toBeInTheDocument();
 });
